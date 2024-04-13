33c704c002a33500b0f26b91c1fa630001c0cd35
Kyle Jacobson <jacokyle01@gmail.com>
2024-04-12 22:39:24 -0500

    add database layer, ODM via mongoose. docs: https://mongoosejs.com/docs/typescript.html

--- backend/controllers/user.ts
+++ backend/controllers/user.ts
@@ -1,5 +1,13 @@
-import { Request, Response } from "express";
+import { Request, Response } from 'express';
+import { User } from '../models/user';
 
 export const allUsers = async (req: Request, res: Response) => {
-    res.json({});
-}
+  // res.json({});
+  try {
+    const users = await User.find();
+    res.json(users);
+  } catch (error) {
+    console.error('Error fetching users:', error);
+    res.status(500).json({ message: 'Server Error' });
+  }
+};
--- backend/models/user.ts
+++ backend/models/user.ts
@@ -1,12 +1,13 @@
-import mongoose from 'mongoose';
+import mongoose, { Document, Schema, model } from 'mongoose';
 
-export const userSchema = new mongoose.Schema({
-  id: {
-    type: Number,
-    required: true,
-  },
-  name: {
-    type: String,
-    required: true,
-  },
+export interface IUser {
+  id: number;
+  name: string;
+}
+
+const userSchema = new Schema<IUser>({
+  id: { type: Number, required: true },
+  name: { type: String, required: true },
 });
+
+export const User = model<IUser>('User', userSchema);
