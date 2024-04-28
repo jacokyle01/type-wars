795014a04e7142231686ba287ae7ef00061d4554
Kyle Jacobson <jacokyle01@gmail.com>
2024-04-27 22:49:45 -0500

    create result model

---
+++ backend/models/result.ts
@@ -0,0 +1,22 @@
+import mongoose, { Document, Schema, model } from 'mongoose';
+import Inc from 'mongoose-sequence';
+const AutoIncrement = Inc(mongoose);
+
+export interface IResult {
+  id: number;
+  uid: number;
+  wpm: number;
+  timeControl: number;
+  createdAt: Date;
+}
+
+const resultSchema = new Schema<IResult>({
+  id: { type: Number, required: true },
+  uid: { type: Number, required: true },
+  wpm: { type: Number, required: true },
+  timeControl: { type: Number, required: true },
+  createdAt: { type: Date, required: true },
+});
+
+resultSchema.plugin(AutoIncrement, { id: 'rid', inc_field: 'id' });
+export const Result = model<IResult>('Result', resultSchema);
