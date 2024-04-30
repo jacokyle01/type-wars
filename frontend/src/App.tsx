f3f5b0958afcb46f9fbfd8ea0c1d003fbce991f9
Kyle Jacobson <jacokyle01@gmail.com>
2024-04-30 12:07:34 -0500

    create some scaffolding for login

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,11 @@
+import { useState } from 'react';
+import { View } from './types/types';
+import { Login } from './components/Login';
+
 const App = () => {
-  return <></>;
+  const [view, setView] = useState<View>('login');
+
+  return view == 'login' ? <Login></Login> : <></>;
 };
 
 export default App;
---
+++ frontend/src/types/types.ts
@@ -0,0 +1,1 @@
+export type View = "login" | "play" | "leaderboard"
