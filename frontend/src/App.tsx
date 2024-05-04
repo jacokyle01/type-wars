7c2fedb51004dc88143869589ec1986b145d999c
Kyle Jacobson <jacokyle01@gmail.com>
2024-05-03 21:25:41 -0500

    implement input handling with onkeydown

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,8 +4,9 @@ import { Login } from './components/Login';
 import { Play } from './components/Play';
 
 const App = () => {
-  const [view, setView] = useState<View>('login');
-  const [name, setName] = useState('');
+  //TODO change 
+  const [view, setView] = useState<View>('play');
+  const [name, setName] = useState('test');
 
   return view == 'login' ? <Login setName={setName} setView={setView}></Login> : <Play name={name}></Play>;
 };
--- frontend/src/components/Play.tsx
+++ frontend/src/components/Play.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { KeyboardEvent, useEffect, useRef, useState } from 'react';
 import { Mode } from '../types/types';
 
 interface PlayProps {
@@ -9,21 +9,29 @@ let interval: number = -1;
 
 export const Play: React.FC<PlayProps> = ({ name }) => {
   const [mode, setMode] = useState<Mode>('notStarted');
+  const inputRef = useRef<HTMLDivElement>(null);
   const [remainingTime, setRemainingTime] = useState(10); //how long time is left. TODO should start at the timeControl
+  useEffect(() => {
+    if (inputRef.current) {
+      inputRef.current.focus();
+    }
+  }, [mode]);
 
   //this is the list of words we are trying to type
   const [targetString, setTargetString] = useState('');
+  //indexes up to what position we have succesfully entered values
+  const [completionIndex, setCompletionIndex] = useState(0);
+  const [lastPressedKey, setLastPressedKey] = useState('');
+
   const [wpm, setWpm] = useState(0);
 
   const handleStart = () => {
     setMode('inProgress'); //updates view
     //TODO make util method that does this
     setTargetString('Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit, optio!');
-
     // setStarted(true);
     // setEnded(false);
     // setInput(quote.quote);
-    // inputRef.current.focus();
     setTimer();
   };
 
@@ -44,6 +52,13 @@ export const Play: React.FC<PlayProps> = ({ name }) => {
     }, 1000);
   };
 
+  const handleKeyDown = (e: KeyboardEvent) => {
+    e.preventDefault();
+    console.log('hello');
+    const { key } = e;
+    setLastPressedKey(key);
+  };
+
   return (
     <>
       <h1>Hello {name}</h1>
@@ -55,7 +70,13 @@ export const Play: React.FC<PlayProps> = ({ name }) => {
             return (
               <>
                 <h1>Time remaining: {remainingTime}</h1>
-                <h3>{targetString}</h3>
+                <div id="test-wrap">
+                  <h3>{targetString}</h3>
+                  <div tabIndex={-1} onKeyDown={handleKeyDown} ref={inputRef}>
+                    Hello
+                  </div>
+                  <h3>Last pressed key: {lastPressedKey}</h3>
+                </div>
               </>
             );
 
