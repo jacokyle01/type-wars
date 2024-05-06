d25756ad2c95029501017c33b1e282dc91cd39f3
Kyle Jacobson <jacokyle01@gmail.com>
2024-05-06 01:32:26 -0500

    reformat play wrap- inline-block to clarify size of border

--- frontend/src/components/Play.tsx
+++ frontend/src/components/Play.tsx
@@ -98,13 +98,13 @@ export const Play: React.FC<PlayProps> = ({ user }) => {
 
   const renderWordCountSelect = () => {
     return (
-      <>
-        <div className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+      <div id='main-wrap' className='text-center'>
+        <div className="mt-10 text-center text-4xl font-bold leading-9 tracking-tight text-gray-900">
           Play
-          <div className="text-gray-500 text-lg font-normal">Complete a typing test</div>
+          <div className="text-gray-500 text-lg font-normal py-2">Complete a typing test</div>
         </div>
-        <div id="word-count-wrap" className="rounded-md ring-2 ring-gray-100 w-1/3 mx-auto">
-          <div className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+        <div id="word-count-wrap" className="inline-block rounded-md ring-2 ring-gray-100 mx-auto my-5 px-3 py-2">
+          <div className="text-center text-2xl font-bold text-gray-900">
             Choose your word count
           </div>
           <div id="select-wrap" className="flex flex-wrap justify-center">
@@ -121,7 +121,7 @@ export const Play: React.FC<PlayProps> = ({ user }) => {
             ))}
           </div>
         </div>
-      </>
+      </div>
     );
   };
 
